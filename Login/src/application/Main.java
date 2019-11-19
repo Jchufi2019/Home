9c02577c544dc3a0a972bd9c0b643cb93d80ce82
Juan Carlos <juancarlospedraza.1990@gmail.com>
2019-11-19 16:23:43 +0100

    actualizar el proyecto

--- Login/src/application/Main.java
+++ Login/src/application/Main.java
@@ -11,14 +11,24 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			FXMLLoader loader = new FXMLLoader(getClass().getResource("View.fxml"));
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("Home.fxml"));
 			AnchorPane root = (AnchorPane) loader.load();
-			Scene scene = new Scene(root,500,500);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			Scene scene = new Scene(root);
+			//scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
+			/**
+			 * FXMLLoader loader = new FXMLLoader(getClass().getResource("Home.fxml"));
+			AnchorPane root = (AnchorPane) loader.load();
+			//BorderPane root = new BorderPane();
+			Scene scene = new Scene(root);
+			//scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			
+			primaryStage.setScene(scene);
+			primaryStage.show();
+			 */
 		}
 	}
 	
