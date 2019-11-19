e47793e33336f05016f0824ee03c31f17bb941fb
Juan Carlos <juancarlospedraza.1990@gmail.com>
2019-11-19 16:27:16 +0100

    Creada rama HomePage

--- Login/src/application/Main.java
+++ Login/src/application/Main.java
@@ -7,6 +7,11 @@ import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 
 
+/**
+ * Rama HomePage
+ * @author juanc
+ *
+ */
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
